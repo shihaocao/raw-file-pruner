6f1dd8cd9a06a838f18fd207986da019ae3b17f8
Shihao Cao <shehowcow@gmail.com>
2022-02-14 01:04:06 -0500

    more documentation

--- raw_file_pruner.py
+++ raw_file_pruner.py
@@ -1,4 +1,4 @@
-'''A python script for pruning raw files.'''
+'''A python script for pruning RAW files that do not have a corresponding JPG in the same directory.'''
 
 
 import argparse
@@ -9,17 +9,29 @@ import re
 
 
 JPG_ENDINGS = [".jpg", ".jpeg"]
-RAW_ENDINGS = [".nef", ".raw"]
+'''All endings that are considered JPG endings, or the endings that is used to determine what is kept.'''
 
+
+RAW_ENDINGS = [".nef", ".raw"]
+'''All the RAW file endings that are considered extraneous and should be pruned if no corresponding JPG is found.'''
 
 class FileHandle:
-    
+    '''A class to handle a file along with relevant information such as base name, full name and file_size.'''
+
     def __init__(self, base_name: str, full_name: str, file_size: int):
+        '''A constructor for the FileHandle class.
+        
+        args:
+            base_name: The base name of the file.
+            full_name: The full name of the file including ending and path
+            file_size: The size of the file in bytes.
+        '''
         self.base_name = base_name
         self.full_name = full_name
         self.file_size = file_size
 
     def __eq__(self, other):
+        '''A method to compare two FileHandles, based on their base name has the same numbering.'''
         if isinstance(other, FileHandle):
             exact = self.base_name.lower() == other.base_name.lower()
             inside = self.base_name.lower() in other.base_name.lower()
@@ -28,6 +40,7 @@ class FileHandle:
         return False
     
     def __hash__(self):
+        '''A method to hash a FileHandle, based on its base name.'''
         return hash(self.base_name)
 
 def parse_args() -> argparse.Namespace:
@@ -42,18 +55,29 @@ def parse_args() -> argparse.Namespace:
 
 
 def find_all_files_with_endings(target_directory: str, endings: List[str]) -> None:
-    '''Find all files with that ending in the current directory, regardless of capitalization'''
+    '''Find all files with that ending in the current directory, regardless of capitalization
+    
+    args:
+        target_directory: The directory to search in
+        endings: the list of endings to consider searching for
+        
+    returns:
+        all file handles that match the search and ending criteria
+    '''
     all_files = os.listdir(target_directory)
     files_with_endings = [x for x in all_files if any(x.lower().endswith(ending) for ending in endings)]
-    
     file_handles = [FileHandle(x[:x.rindex(".")], x, os.path.getsize(target_directory + '/' + x)) for x in files_with_endings]
     
     return file_handles
 
 
 def find_all_directories_without_hidden(target: dir) -> List[str]:
+    '''Find all directories in the target directory, and ignore hidden directories
+    
+    returns:
+        all the directories in the target directory, excluding hidden directories
+    '''
     all_directories = [x[0] for x in os.walk(target)]
-
     HIDDEN = r".*\.\w+"
     
     # find all directories that don't have pass through a hidden folder
@@ -61,13 +85,13 @@ def find_all_directories_without_hidden(target: dir) -> List[str]:
 
 
 def delete_file(file_name: str) -> None:
-    '''Delete the file'''
+    '''Delete the at the given absolute path'''
     os.remove(file_name)
     print(f"Deleted file: {file_name}")
 
 
 def find_and_prune(target: dir, recursive: bool = False, delete: bool = False) -> int:
-    '''Find all heic files in the target directory, and convert them'''
+    '''Find all RAW files in the target directory, and convert them'''
     
     num_deleted = 0
     total_files_size = 0
